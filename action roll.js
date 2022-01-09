00dd2baecc4ebd05ad59f52495e6373e8cee09bf
Wayne Kueh <wayneku88@gmail.com>
2022-01-08 19:08:21 -0500

    Token select check and action list for action and fortune rolls

--- action roll.js
+++ action roll.js
@@ -1,15 +1,53 @@
 // ICON Narrative Play Action Roll
+const controlled = canvas.tokens.controlled;
+if (controlled.length <= 0){
+	new Dialog({
+		title: "Hold up",
+		content: `<h3>You don't have your token selected!</h3>`,
+		buttons: {
+			oops: {
+				icon: '<i class="fas fa-frown"></i>',
+				label: "Oops..."
+			}
+		}
+	}).render(true);
+	return;
+}
+const actionList = [
+`ACTION`,
+`Sneak`,
+`Excel`,
+`Survey`,
+`Channel`,
+`Charm`,
+`Command`,
+`Tinker`,
+`Study`,
+`Recall`,
+`Traverse`,
+`Smash`,
+`Endure`,
+`Skirmish`,
+`Snipe`,
+`Crush`
+];
+let actionsText = ``;
+actionList.forEach(function(item){
+	actionsText += `<option value="${item}">${item}</option>`
+});
 let optionsText = `<option value=0>0 [2d6kl]</option>`;
 let i = 1;
 for (; i < 10; i++) {
     optionsText += `<option value="${i}">${i}</option>`;
 }
-
 let confirmed = false;
 let boonmod = "";
 new Dialog({
     title: "Action Roll",
     content: `
+			<form id="action-form"> <div class="form-group"> <label>Action</label> <div class="form-fields">
+            <select name="action-type">` + actionsText + `</select>
+            </div> </div> </form>
             <form id="action-form"> <div class="form-group"> <label># of Dice</label> <div class="form-fields">
             <select name="dice-number">` + optionsText + `</select>
             </div> </div> </form> `,
@@ -29,10 +67,11 @@ default: "Cancel",
     close: html => {
     if(confirmed){
         let boonmod = parseInt(html.find('[name=dice-number]')[0].value);
-        disp(boonmod);}
+		let act_type = html.find('[name=action-type]')[0].value;
+        disp(boonmod, act_type);}
     }}).render(true); 
 
-async function disp(boons){
+async function disp(boons, act_type){
 	let rollterm = ``;
 	if(boons === 0){
 		rollterm = `2d6kl`;
@@ -41,11 +80,8 @@ async function disp(boons){
 	} else {
 		rollterm = boons+`d6kh`;
 	}
-	
 	let r = new Roll(rollterm).roll({async:false});
-	
 	let result_html = ChatMessage.getSpeaker({token:actor}).alias + ` `;
-	
 	switch(r.total){
 		case 1:
 		case 2:
@@ -63,8 +99,8 @@ directly and suffers the full consequences`
 		default:
 			result_html += `??`
 	};
-
 	await r.toMessage({
+		flavor: `<h2 style="text-align:center">` + act_type + ` roll</h2>`,
 		speaker: ChatMessage.getSpeaker({token:actor}),
 		content: await r.render()+result_html,
 	});
--- fortune roll.js
+++ fortune roll.js
@@ -1,15 +1,53 @@
 // ICON Narrative Play Fortune Roll
+const controlled = canvas.tokens.controlled;
+if (controlled.length <= 0){
+	new Dialog({
+		title: "Hold up",
+		content: `<h3>You don't have your token selected!</h3>`,
+		buttons: {
+			oops: {
+				icon: '<i class="fas fa-frown"></i>',
+				label: "Oops..."
+			}
+		}
+	}).render(true);
+	return;
+}
+const actionList = [
+`ACTION`,
+`Sneak`,
+`Excel`,
+`Survey`,
+`Channel`,
+`Charm`,
+`Command`,
+`Tinker`,
+`Study`,
+`Recall`,
+`Traverse`,
+`Smash`,
+`Endure`,
+`Skirmish`,
+`Snipe`,
+`Crush`
+];
+let actionsText = ``;
+actionList.forEach(function(item){
+	actionsText += `<option value="${item}">${item}</option>`
+});
 let optionsText = `<option value=0>0 [2d6kl]</option>`;
 let i = 1;
 for (; i < 10; i++) {
     optionsText += `<option value="${i}">${i}</option>`;
 }
-
 let confirmed = false;
 let boonmod = "";
 new Dialog({
     title: "FORTUNE Roll",
     content: `
+			<form id="action-form"> <div class="form-group"> <label>Action</label> <div class="form-fields">
+            <select name="action-type">` + actionsText + `</select>
+            </div> </div> </form>
             <form id="fortune-form"> <div class="form-group"> <label># of Dice</label> <div class="form-fields">
             <select name="dice-number">` + optionsText + `</select>
             </div> </div> </form> `,
@@ -29,7 +67,8 @@ default: "Cancel",
     close: html => {
     if(confirmed){
         let boonmod = parseInt(html.find('[name=dice-number]')[0].value);
-        disp(boonmod);}
+		let act_type = html.find('[name=action-type]')[0].value;
+        disp(boonmod, act_type);}
     }}).render(true); 
 
 async function disp(boons){
@@ -64,6 +103,7 @@ async function disp(boons){
 	};
 
 	await r.toMessage({
+		flavor: `<h2 style="text-align:center">` + act_type + ` roll</h2>`,
 		speaker: ChatMessage.getSpeaker({token:actor}),
 		content: await r.render()+result_html,
 	});
